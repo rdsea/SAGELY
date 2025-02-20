8a8ae93b5222802628b79bb707193b438fbca04e
hong3nguyen <tringuyennht@gmail.com>
2025-02-20 09:19:43 +0200

    add config editting

--- drone/src/drone/setup.py
+++ drone/src/drone/setup.py
@@ -9,6 +9,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", ["launch/drone_launch.py"]),
     ],
     install_requires=[
         "setuptools",
@@ -30,6 +31,7 @@ setup(
     entry_points={
         "console_scripts": [
             "client = client_drone.client:main",
+            "drone_data_sender = client_drone.drone_data_sender:main",
         ],
     },
 )
