406f793d43d852efd686a0c93f48b5e2384a4f74
hong3nguyen <tringuyennht@gmail.com>
2025-03-28 18:54:51 +0200

    update setting for a set of drones working together

---
+++ drone/px4_gazebo/docker_run_commands.sh
@@ -0,0 +1,4 @@
+docker run --name=px4-drone101 --net px4net --ip 192.168.132.101 px4_gaze
+docker run --name=px4-drone102 --net px4net --ip 192.168.132.102 px4_gaze
+docker run --name=px4-drone103 --net px4net --ip 192.168.132.103 px4_gaze
+docker run --name=px4-drone104 --net px4net --ip 192.168.132.104 px4_gaze
---
+++ drone/px4_gazebo/px4-drone103_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define variable values for px4-drone103
+export NAME="ros2_px4_gazebo"
+export ROOT="/root"
+export GZ_PARTITION="relay"
+export GZ_IP="192.168.132.1"
+export PX4_IP="192.168.132.103"
+export PX4_MODEL="gz_x500"
+
+# Replace variables in the template and generate the final config.yaml
+envsubst < /path/to/config_template.yaml > /path/to/px4-drone103_config.yaml
+
+echo "Generated config.yaml successfully for px4-drone103 with IP 192.168.132.103!"
