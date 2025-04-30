99f9e44227a173caf60f0d371a51a8c724f86df8
hong3nguyen <tringuyennht@gmail.com>
2025-04-30 12:05:16 +0300

    update the envoy and opa to each drone, edit the configuration from envoy.yaml, but not actually understand grpc here and some microsoft configuration

--- drone/px4_gazebo/docker-compose-Adrone.sh
+++ drone/px4_gazebo/docker-compose-Adrone.sh
@@ -7,15 +7,16 @@ DRONE_MODEL="gz_x500"
 START_IP=101
 END_IP=104
 BASE_IP="192.168."
-SWARM_IP=132
+SWARM_SUBNET=132
+
+CONTAINER=hongtringuyen/gazebo_sim_px4
 
 SWARM_CONTAINER=hello-world
-CONTAINER=hongtringuyen/gazbo_px4_ros2:v1
-ROS2_CONTAINER=abc
+# application services
+ROS2_CONTAINER=ros:humble-ros-base-jammy
 
 NETWORK_NAME=swarm_net
 
-ENVOY_CONFIG="./envoy/config.yaml"
 OPA_POLICY="./opa/policy.rego"
 
 #echo "networks:" >docker_net.yml
@@ -28,16 +29,22 @@ OPA_POLICY="./opa/policy.rego"
 #         - subnet: 192.168.132.0/24
 # EOF
 
-docker network create --driver bridge --subnet "${BASE_IP}${SWARM_IP}.0/24" "${NETWORK_NAME}"
+docker network create --driver bridge --subnet "${BASE_IP}${SWARM_SUBNET}.0/24" "${NETWORK_NAME}"
 
 # Loop to generate docker-compose files and drone configs
 for ((i = START_IP; i <= END_IP; i++)); do
   DRONE_NAME="drone_${i}"
   SWARM_NAME="swarm_${i}"
-  DRONE_IP="${BASE_IP}${SWARM_IP}.${i}"
+  ROS2_NAME="ros2_${i}"
+
+  DRONE_IP="${BASE_IP}${SWARM_SUBNET}.${i}"
+  SWARM_IP="${BASE_IP}${SWARM_SUBNET}.$((i + 10))"
+
   COMPOSE_FILE="docker-compose-${DRONE_NAME}.yml"
   CONFIG_FILE="configs/${DRONE_NAME}.yml"
 
+  ENVOY_FILE="./envoy/${DRONE_NAME}.yml"
+  ENVOY_CONFIG="./envoy/config.yaml"
   docker network create --driver bridge --subnet "${BASE_IP}${i}.0/24" "${DRONE_NAME}_net"
 
   #   cat <<EOF >>docker_net.yml
@@ -60,26 +67,12 @@ networks:
     external: true
 
 services:
-  ${DRONE_NAME}:
-    image: ${CONTAINER} 
-    container_name: ${DRONE_NAME}
-    networks:
-      ${NETWORK_NAME}:
-        ipv4_address: ${DRONE_IP}
-      ${DRONE_NAME}_net:
-        ipv4_address: ${BASE_IP}${i}.1
-    volumes:
-      - ./${CONFIG_FILE}:/root/.config/tmuxinator/px4_ros2_gazebo.yml
-    environment:
-      GZ_PARTITION: "${GZ_PARTITION}"
-      GZ_IP: "${GZ_IP}"
-      PX4_MODEL: "${DRONE_MODEL}"
-    command: ["/bin/bash", "-c", "tmuxinator start -p /root/.config/tmuxinator/px4_ros2_gazebo.yml"]
 
-  envoy:
+  envoy_${i}:
     image: envoyproxy/envoy:v1.23-latest
+    container_name: envoy_${i}
     volumes:
-      - ${ENVOY_CONFIG}:/etc/envoy/envoy.yaml
+      - ${ENVOY_FILE}:/etc/envoy/envoy.yaml
     ports:
       - "5200:8000"
       - "5201:8001"
@@ -87,8 +80,9 @@ services:
       ${DRONE_NAME}_net:
         ipv4_address: ${BASE_IP}${i}.2
 
-  opa:
+  opa_${i}:
     image: openpolicyagent/opa:latest-envoy
+    container_name: opa_${i}
     volumes:
       - ${OPA_POLICY}:/policy/policy.rego
     command:
@@ -107,19 +101,51 @@ services:
       ${DRONE_NAME}_net:
         ipv4_address: ${BASE_IP}${i}.3
 
-  ${SWARM_NAME}:
-    image: ${SWARM_CONTAINER} 
-    container_name: ${SWARM_NAME}
+  ${DRONE_NAME}:
+    image: ${CONTAINER} 
+    container_name: ${DRONE_NAME}
     networks:
       ${NETWORK_NAME}:
         ipv4_address: ${DRONE_IP}
+      ${DRONE_NAME}_net:
+        ipv4_address: ${BASE_IP}${i}.${i}
     volumes:
       - ./${CONFIG_FILE}:/root/.config/tmuxinator/px4_ros2_gazebo.yml
     environment:
       GZ_PARTITION: "${GZ_PARTITION}"
       GZ_IP: "${GZ_IP}"
       PX4_MODEL: "${DRONE_MODEL}"
-    command: ["/bin/bash", "-c", "tmuxinator start -p /root/.config/tmuxinator/px4_ros2_gazebo.yml"]
+    #command: ["/bin/bash", "-c", "tmuxinator start -p /root/.config/tmuxinator/px4_ros2_gazebo.yml"]
+    command: ["tmuxinator", "start", "px4_ros2_gazebo.yml"]
+
+  ${SWARM_NAME}:
+    image: ${SWARM_CONTAINER} 
+    container_name: ${SWARM_NAME}
+    networks:
+      ${NETWORK_NAME}:
+        ipv4_address: ${SWARM_IP}
+    volumes:
+      - ./${CONFIG_FILE}:/root/.config/tmuxinator/px4_ros2_gazebo.yml
+    environment:
+      GZ_PARTITION: "${GZ_PARTITION}"
+      GZ_IP: "${GZ_IP}"
+      PX4_MODEL: "${DRONE_MODEL}"
+    #command: ["/bin/bash", "-c"] # , "tmuxinator start -p /root/.config/tmuxinator/px4_ros2_gazebo.yml"]
+
+  ${ROS2_NAME}:
+    image: ${ROS2_CONTAINER} 
+    container_name: ${ROS2_NAME}
+    networks:
+      ${DRONE_NAME}_net:
+        ipv4_address: ${BASE_IP}${i}.4
+    # volumes:
+    #   - ./${CONFIG_FILE}:/root/.config/tmuxinator/px4_ros2_gazebo.yml
+    # environment:
+    #   GZ_PARTITION: "${GZ_PARTITION}"
+    #   GZ_IP: "${GZ_IP}"
+    #   PX4_MODEL: "${DRONE_MODEL}"
+    command: ["/bin/bash", "-c"] # , "tmuxinator start -p /root/.config/tmuxinator/px4_ros2_gazebo.yml"]
+
 EOF
 
   # Generate unique config files for each drone
@@ -148,6 +174,143 @@ EOF
 
   echo "Generated ${COMPOSE_FILE} and ${CONFIG_FILE}"
 
+  mkdir -p envoy
+  cat <<EOF >${ENVOY_FILE}
+static_resources:
+  listeners:
+    - address:
+        socket_address:
+          address: 0.0.0.0
+          port_value: 8000
+      filter_chains:
+        - filters:
+            - name: envoy.filters.network.http_connection_manager
+              typed_config:
+                "@type": type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager
+                codec_type: auto
+                stat_prefix: ingress_http
+                route_config:
+                  name: local_route
+                  virtual_hosts:
+                    - name: backend
+                      domains:
+                        - ["*"]
+                      routes:
+                        - match:
+                            prefix: "/"
+                          route:
+                            cluster: service
+                http_filters:
+                  - name: envoy.filters.http.jwt_authn
+                    typed_config:
+                      "@type": type.googleapis.com/envoy.extensions.filters.http.jwt_authn.v3.JwtAuthentication
+                      providers:
+                        provider_azad:
+                          issuer: https://login.microsoftonline.com/<TenantID>/v2.0
+                          audiences: <ClientID>
+                          forward: true
+                          remote_jwks:
+                            http_uri:
+                              uri: "https://login.microsoftonline.com/<TenantID>/discovery/v2.0/keys"
+                              cluster: azad
+                              timeout: 10s
+                            cache_duration:
+                              seconds: 600
+                      rules:
+                        - match:
+                            prefix: /health
+                        - match:
+                            prefix: /common
+                          requires:
+                            provider_and_audiences:
+                              provider_name: provider_azad
+                              audiences: <ClientID>
+                        - match:
+                            prefix: /workspaces
+                          requires:
+                            provider_and_audiences:
+                              provider_name: provider_azad
+                              audiences: <ClientID>
+                  - name: envoy.ext_authz
+                    typed_config:
+                      "@type": type.googleapis.com/envoy.extensions.filters.http.ext_authz.v3.ExtAuthz
+                      transport_api_version: V3
+                      with_request_body:
+                        max_request_bytes: 8192
+                        allow_partial_message: true
+                      failure_mode_allow: false
+                      grpc_service:
+                        envoy_grpc:
+                          cluster_name: envoy.ext_authz
+                        timeout: 0.5s
+                        # google_grpc:
+                        #   target_uri: ${BASE_IP}${i}.3:9191
+                        #   stat_prefix: ext_authz
+                  - name: envoy.filters.http.router
+                    typed_config:
+                      "@type": type.googleapis.com/envoy.extensions.filters.http.router.v3.Router
+  clusters:
+    - name: service
+      connect_timeout: 0.25s
+      type: strict_dns
+      lb_policy: round_robin
+      load_assignment:
+        cluster_name: service
+        endpoints:
+          - lb_endpoints:
+              - endpoint:
+                  address:
+                    socket_address:
+                      address: ${BASE_IP}${i}.${i}
+                      port_value: 8080
+    - name: envoy.ext_authz
+      connect_timeout: 0.25s
+      type: strict_dns
+      lb_policy: round_robin
+      load_assignment:
+        cluster_name: ext_authz
+        endpoints:
+          - lb_endpoints:
+              - endpoint:
+                  address:
+                    socket_address:
+                      address: ${BASE_IP}${i}.3
+                      port_value: 9191
+    - name: azad
+      connect_timeout: 100s
+      type: strict_dns
+      lb_policy: round_robin
+      load_assignment:
+        cluster_name: azad
+        endpoints:
+          - lb_endpoints:
+              - endpoint:
+                  address:
+                    socket_address:
+                      address: login.microsoftonline.com
+                      port_value: 443
+      transport_socket:
+        name: envoy.transport_sockets.tls
+        typed_config:
+          "@type": type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.UpstreamTlsContext
+          sni: login.microsoftonline.com
+admin:
+  address:
+    socket_address:
+      address: 0.0.0.0
+      port_value: 8001
+layered_runtime:
+  layers:
+    - name: static_layer_0
+      static_layer:
+        envoy:
+          resource_limits:
+            listener:
+              example_listener_name:
+                connection_limit: 10000
+        overload:
+          global_downstream_max_connections: 50000
+EOF
 done
 
 echo "Generated all docker-compose files and configuration files in ./configs/"
