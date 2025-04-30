2e1b4461d347eb396cf878b22242115026f17d6a
hong3nguyen <tringuyennht@gmail.com>
2025-04-30 19:18:13 +0300

    update error

--- drone/px4_gazebo/keyboard_control/KeyPressModule.py
+++ drone/px4_gazebo/keyboard_control/KeyPressModule.py
@@ -3,7 +3,9 @@ import pygame
 
 def init():
     pygame.init()
-    win = pygame.display.set_mode((200, 200))
+    #win = pygame.display.set_mode((200, 200))
+    pygame.display.set_mode((200, 200))
+
 
 
 def getKey(keyName):
