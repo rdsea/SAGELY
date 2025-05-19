ae44b11570df1ec96e10e4eb50406eeee8b50321
nguu0123 <nguu0123@gmail.com>
2025-05-19 16:06:22 +0300

    Enhance pre-commit and remove deprecated Tiltfiles
    - Added ans to the spell check ignore list in the pre-commit config.
    - Deleted unused Tiltfiles for scaling applications.
    - Created README for drone simulation setup with PX4 and Gazebo.
    - Improved quoting in shell scripts for robustness.
    - Removed redundant dependency comments in lock files.

--- drone/px4_gazebo/cleaning.sh
+++ drone/px4_gazebo/cleaning.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker network rm $(docker network ls -q)
+docker stop "$(docker ps -aq)"
+docker rm "$(docker ps -aq)"
+docker network rm "$(docker network ls -q)"
 
 ./docker-compose-Adrone.sh
 # docker container prune
