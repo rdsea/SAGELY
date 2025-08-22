25b35b3b3419dcc70c89254187fa349f7cdfa63b
nguu0123 <nguu0123@gmail.com>
2025-08-22 14:50:38 +0300

    Update framework name from holistic_policy to sagely

--- infrastructure/deploy_istio.sh
+++ infrastructure/deploy_istio.sh
@@ -8,23 +8,23 @@ kubectl label namespace default opa-istio-injection="enabled"
 cd ..
 
 # OPA admission
-kubectl apply -f src/holistic_policy/k8s_deployment/istio-opa/opa_controller.yml
-kubectl apply -f src/holistic_policy/k8s_deployment/istio-opa/opa_authz.yml
-kubectl apply -f src/holistic_policy/k8s_deployment/istio-opa/opa_config.yml
+kubectl apply -f src/sagely/k8s_deployment/istio-opa/opa_controller.yml
+kubectl apply -f src/sagely/k8s_deployment/istio-opa/opa_authz.yml
+kubectl apply -f src/sagely/k8s_deployment/istio-opa/opa_config.yml
 
 # enable external provider: jaeger and OPA
-istioctl install -f src/holistic_policy/k8s_deployment/monitoring/external_provider.yml --skip-confirmation
-kubectl apply -f src/holistic_policy/k8s_deployment/monitoring/enable_tracing.yml
-kubectl apply -f src/holistic_policy/k8s_deployment/monitoring/prometheus.yml
+istioctl install -f src/sagely/k8s_deployment/monitoring/external_provider.yml --skip-confirmation
+kubectl apply -f src/sagely/k8s_deployment/monitoring/enable_tracing.yml
+kubectl apply -f src/sagely/k8s_deployment/monitoring/prometheus.yml
 
 # deploy JAEGER
-kubectl apply -f src/holistic_policy/k8s_deployment/monitoring/jaeger_istio.yml
+kubectl apply -f src/sagely/k8s_deployment/monitoring/jaeger_istio.yml
 
 # Apply routing rule
-kubectl apply -f src/holistic_policy/k8s_deployment/policy_deployment/platform_context.yml
-kubectl apply -f src/holistic_policy/k8s_deployment/policy_deployment/service_discovery.yml
-kubectl apply -f src/holistic_policy/k8s_deployment/policy_deployment/context_management.yml
+kubectl apply -f src/sagely/k8s_deployment/policy_deployment/platform_context.yml
+kubectl apply -f src/sagely/k8s_deployment/policy_deployment/service_discovery.yml
+kubectl apply -f src/sagely/k8s_deployment/policy_deployment/context_management.yml
 
-kubectl apply -f src/holistic_policy/k8s_deployment/istio/application_gateway.yml
-kubectl apply -f src/holistic_policy/k8s_deployment/istio/virtual_services.yml
-kubectl apply -f src/holistic_policy/k8s_deployment/istio/destination_rules.yml
+kubectl apply -f src/sagely/k8s_deployment/istio/application_gateway.yml
+kubectl apply -f src/sagely/k8s_deployment/istio/virtual_services.yml
+kubectl apply -f src/sagely/k8s_deployment/istio/destination_rules.yml
