bbbd0096c0fc5ba3a779497e5010231eff51d506
hong3nguyen <tringuyennht@gmail.com>
2025-09-01 22:18:20 +0300

    clean unsuage and redandant code

--- swarm_simulation/scripts/cleaning.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker stop "$(docker ps -aq)"
-docker rm "$(docker ps -aq)"
-docker network rm "$(docker network ls -q)"
-
-#./docker-compose-Adrone.sh
-# docker container prune
-# docker network prune
